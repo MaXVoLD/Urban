0a22f3134b6e7a674dc336ac95ccc626af81d2ce
MaXVoLD <165142130+MaXVoLD@users.noreply.github.com>
2024-07-11 20:00:51 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,19 @@
+quantity_homework = float(12)
+quantity_hours = float(1.5)
+name_course = 'Python'
+time_of_task = (quantity_hours / quantity_homework)
+print('\n Курс: ', name_course,
+      '\n Всего задач: ', quantity_homework,
+      '\n Затрачено часов: ', quantity_hours,
+      '\n Среднее время выполнения задания: ', time_of_task)
+
+# Вариант через "innput"
+
+# quantity_homework = float(input('Введите количество выполненных ДЗ: '))
+# quantity_hours = float(input('Введите количество затраченных часов: '))
+# name_course = 'Python'
+# time_of_task = (quantity_hours / quantity_homework)
+# print('\n Курс: ', name_course,
+#       '\n Всего задач: ', quantity_homework,
+#       '\n Затрачено часов: ', quantity_hours,
+#       '\n Среднее время выполнения задания: ', time_of_task)
