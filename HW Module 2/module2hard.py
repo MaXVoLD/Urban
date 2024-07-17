344163e99bc5ec20c794e1572a18d90ab3b19b65
MaXVoLD <165142130+MaXVoLD@users.noreply.github.com>
2024-07-17 16:32:00 +0300

    Add File

---
+++ HW Module 2/module2hard.py
@@ -0,0 +1,24 @@
+def pass_word(n=20):  # Значение по умолчанию установил для удобства проверки дебагом, комментируя строку input
+    list_value = []  # Список для хранения списков пар значений
+    list_password = []  # Список для хранения значиний после объеденения списков внутри list_value
+    password = 0  # переменная для хранения значений из списка list_password
+    n = int(input('Введите число от 3 до 20: '))
+    if n < 3 or n > 20:
+        print('''
+Введено неверное значние,
+повторите попытку.
+        ''')
+        pass_word()
+    else:
+        for i in range(1, n):
+            for j in range(1, n):
+                if n % (i + j) == 0 and i != j and [j, i] not in list_value:
+                    list_value.append([i, j])  # подобрал уникальные значения пар
+
+    for i in list_value:
+        list_password.extend(i)  # распаковал внутренние списки и объеденил их в один список
+        password = ''.join(str(x) for x in list_password)  # перебрал значения из списка, присвоил их переменой
+    print(password)
+
+
+pass_word()
