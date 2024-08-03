d992b387aa58bee179e43e2fad4bf9a8835a4003
MaXVoLD <165142130+MaXVoLD@users.noreply.github.com>
2024-08-03 12:52:18 +0300

    Добавил элемент рандома, рассписал подробнее логику

--- main2.py
+++ main2.py
@@ -1,15 +1,15 @@
 import random
 
-random_value = random.randint(3, 20)
+random_number = random.randint(3, 20)
 
 
 def pass_word():
     list_value = []  # Список для хранения списков пар значений
     list_password = []  # Список для распаковки list_value
-    print(f'Случайное число: {random_value}')
-    for i in range(1, random_value):
-        for j in range(1, random_value):
-            if all([random_value % (i + j) == 0,  # Сумма пар должна быть кратна random_number
+    print(f'Случайное число: {random_number}')
+    for i in range(1, random_number):
+        for j in range(1, random_number):
+            if all([random_number % (i + j) == 0,  # Сумма пар должна быть кратна random_number
                     i != j,  # Пара не должна состоять из одинаковых чисел
                     [j, i] not in list_value]):  # Пара не должна быть реверсивной [[3 , 1] , [1 , 3]]
                 list_value.append([i, j])  # Добавляем уникальные пары в list_value
